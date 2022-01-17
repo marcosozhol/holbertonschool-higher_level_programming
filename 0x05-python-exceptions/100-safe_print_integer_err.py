ae38b8d92b50e052eb30aba72dc06b7cecb5e682
marcosozhol <3956@holbertonschool.com>
2022-01-17 13:50:22 -0800

    Task 100 - function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except TypeError as error:
-        print('Exception: {})'.format(error), file=sys.stderr)
+        print('Exception: {}'.format(error), file=sys.stderr)
         return False
     except ValueError as error_1:
         print('Exception: {}'.format(error_1), file=sys.stderr)
