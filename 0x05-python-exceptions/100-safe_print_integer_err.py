e9f4196c08430adc145063f377eaaaac4054f816
marcosozhol <3956@holbertonschool.com>
2022-01-17 13:25:48 -0800

    Task 100 - function that prints an integer

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,9 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
+    except TypeError:
+        print("Exception: Unknown format code 'd' for object of type 'str'"    , file=sys.stderr)
+        return False
     except ValueError:
         print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
         return False
