429f9170abe33c70f40f5ca1af1f8dde8cbc1079
Marcosozhol <3956@holbertonschool.com>
2022-01-19 05:31:29 -0800

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """
+    Create a Square class
+    """
+    def __init__(self, size = 0):
+        """
+        Private instance attribute: size.
+        Instantiation with size (no type/value verification).
+        """
+        if(type(size) != int):
+            print("size must be an integer", end="")
+            raise TypeError
+
+        if(size < 0):
+            print("size must be >= 0", end="")
+            raise ValueError
+
+        self.__size = size
