7efcb798f45754007db69e90ea63cca07224e63d
Marcosozhol <3956@holbertonschool.com>
2022-01-19 05:05:43 -0800

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""class Square"""
+
+
 class Square:
     """
     Create a Square class
     """
-    def __init__(self, size=0):
+    def __init__(self, size=None):
         """
         Private instance attribute: size.
         Instantiation with size (no type/value verification).
