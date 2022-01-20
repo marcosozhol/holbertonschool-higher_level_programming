447fa36d5d64175600549fbeec21f0a9cf6e5a1c
Marcosozhol <3956@holbertonschool.com>
2022-01-20 07:07:59 -0800

    Task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -38,23 +38,6 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    def my_print(self):
-        """prints in stdout the square with the character #"""
-        if(self.__size == 0):
-            print()
-        else:
-            for i in range(self.__position[1]):
-                print()
-
-        for i in range(self.__size):
-            for i in range(self.__position[0]):
-                print(" ", end="")
-
-            print("#" * self.__size)
-
-        if(self.__size == 0):
-            print("")
-
     @property
     def position(self):
         """"retrieve it"""
@@ -70,3 +53,17 @@ class Square:
         if(position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #"""
+        if(self.__size == 0):
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+
+        for i in range(self.__size):
+            for i in range(self.__position[0]):
+                print(" ", end="")
+
+            print("#" * self.__size)
