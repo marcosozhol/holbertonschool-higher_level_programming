4acf5aeb20d602c10a42da1b65b466442e834064
Marcosozhol <3956@holbertonschool.com>
2022-01-24 06:59:54 -0800

    Task 1 - add private instance attribute height and width

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,17 +1,37 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Define a empty class"""
 
 
 class Rectangle:
     """Create class Rectangle"""
 
-    def __init__(self, width=0, heigth=0):
+    def __init__(self, width=0, height=0):
         """Private instance attribute: width"""
+
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """property def heigth(self): to retrieve it"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """heigth must be an integer and > 0"""
+        if(type(value) != int):
+            raise TypeError("height must be an integer")
+
+        elif(value < 0):
+            raise ValueError("height must be >= 0")
+
+        else:
+            self.__height = value
 
     @property
     def width(self):
         """property def width(self): to retrieve it"""
-        self.__width = width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -19,19 +39,8 @@ class Rectangle:
         if(type(value) != int):
             raise TypeError("width must be an integer")
 
-        if(value < 0):
+        elif(value < 0):
             raise ValueError("width must be >= 0")
 
-    @property
-    def heigth(self):
-        """property def heigth(self): to retrieve it"""
-        self.__heigth = heigth
-
-    @heigth.setter
-    def heigth(self, value):
-        """heigth must be an integer and > 0"""
-        if(type(value) != int):
-            raise TypeError("height must be an integer")
-
-        if(value < 0):
-            raise ValueError("height must be >= 0")
+        else:
+            self.__width = value
