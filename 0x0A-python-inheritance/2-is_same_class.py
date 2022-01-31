2b06880c6ec9717022d6f746b18bc339e4513a5a
Marcosozhol <3956@holbertonschool.com>
2022-01-31 14:05:17 -0800

    Task 2 - the object is exactly an instance of the specified class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Task 2 - """
+
+
+def is_same_class(obj, a_class):
+    """
+    the object is exactly
+    an instance of the
+    specified class
+    """
+    return type(obj) == a_class
