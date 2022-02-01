fb982ed380bd52fb32d5d366a28110d17d3ad451
Marcosozhol <3956@holbertonschool.com>
2022-02-01 14:07:03 -0800

    Task 5 - writes an Object to a text file, using a JSON representation

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Module """
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """  writes an Object to a text file,
+    using a JSON representation
+    """
+    with open(filename, 'w', encoding="utf-8") as file:
+        file.write(json.dumps(my_obj))
