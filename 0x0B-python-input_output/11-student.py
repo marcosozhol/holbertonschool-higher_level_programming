16507829c7bedd10163263b472e6d9f250b93140
Marcosozhol <3956@holbertonschool.com>
2022-02-02 11:09:53 -0800

    Task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class Student:
+    """A class named Student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Dictionary representation of student"""
+        if type(attrs) != list:
+            return self.__dict__
+
+        new_dict = {}
+
+        for i in attrs:
+            if type(i) != str:
+                return self.__dict__
+            if i in self.__dict__:
+                new_dict[i] = self.__dict__[i]
+
+        return new_dict
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student instance"""
+        for i in json:
+            self.__dict__[i] = json[i]
