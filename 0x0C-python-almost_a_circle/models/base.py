e487ca245025f17c2c7a4923f426d94303328307
Marcosozhol <3956@holbertonschool.com>
2022-02-07 12:13:30 -0800

    Actualizado con def to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 """ Module class Base """
 
+import json
+
 
 class Base:
     """ Class Base """
     __nb_objects = 0
 
-
-    def __init__(self, id = None):
+    def __init__(self, id=None):
         """ Initialize class Base """
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Static method that returns the JSON string """
+        if list_dictionaries is None:
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
