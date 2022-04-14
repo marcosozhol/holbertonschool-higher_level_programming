f2eca11640a7a81947a1a08f8031664b380665ed
Marcosozhol <3956@holbertonschool.com>
2022-04-14 13:46:38 -0700

    Correction coments

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,16 +3,14 @@
 lists all states with a name starting with N
 (upper N) from the database hbtn_0e_0_usa
 """
-
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
     """
     Results must be sorted in ascending order by states.id
     Results must be displayed as they are in the example below
     """
-    import MySQLdb
-    import sys
-
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
