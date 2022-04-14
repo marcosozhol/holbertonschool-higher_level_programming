e3a8b1ec9eee848a467ac3f653612d67aa990d2c
Marcosozhol <3956@holbertonschool.com>
2022-04-14 13:50:51 -0700

    Correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,10 +15,7 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY `id`")
-    cur.fetchall()
-    for state in state:
-        if state[1][0] == "N":
-            print(state)
-
+    [print(state) for state in cur.fetchall() if state[1][0] == "N"]
+    
     cur.close()
     db.close()
