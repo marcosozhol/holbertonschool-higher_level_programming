03e03cf4e06f8871aa99b407f71b448af0d73b02
Marcosozhol <3956@holbertonschool.com>
2022-04-14 13:37:08 -0700

    Correction name of file

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states`")
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
     cur.fetchall()
     for state in state:
         if state[1][0] == "N":
