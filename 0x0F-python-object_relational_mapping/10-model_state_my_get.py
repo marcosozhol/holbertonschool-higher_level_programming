1c09c4b9e494743fc1fbc5a7ca580776a0b4891c
Marcosozhol <3956@holbertonschool.com>
2022-04-15 07:57:16 -0700

    Task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""adds the State object “Louisiana”
-to the database hbtn_0e_6_usa"""
+"""prints the State object with the name
+passed as argument from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
 
