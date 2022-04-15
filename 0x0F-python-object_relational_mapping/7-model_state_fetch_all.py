582c77ebc089bb106b1be3085a628e7c03139257
Marcosozhol <3956@holbertonschool.com>
2022-04-15 06:30:08 -0700

    Correction

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,10 +3,12 @@
 lists all State objects
 from the database hbtn_0e_6_usa
 """
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from model_state import Base, State
 
 if __name__ == "__main__":
 
