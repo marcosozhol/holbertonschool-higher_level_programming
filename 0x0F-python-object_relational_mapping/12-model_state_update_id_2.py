8579460b72811bd9b9088a13e12df96927a5de8a
Marcosozhol <3956@holbertonschool.com>
2022-04-15 08:07:59 -0700

    Correction code

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
+    from sqlalchemy.orm import Table
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
@@ -15,7 +16,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    state = session.query(State).filter(State.name == 2).first()
+    state = session.query(State).filter(State.id == 2).first()
     state.name = 'New Mexico'
     session.commit()
     session.close()
