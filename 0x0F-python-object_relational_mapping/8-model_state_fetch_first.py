c570b7d0c94b54d30a54b68f5265cb453986b1db
Marcosozhol <3956@holbertonschool.com>
2022-04-15 19:29:53 -0700

    Correction error

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-prints the first State object
-from the database hbtn_0e_6_usa
+script that lists all State objects from a database
 """
 
 
-import sys
+from model_state import Base, State
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
-if __name__ == "__main__":
-
-    engine = create_engine("mysql+mysqldb://{}:{}@localhst/{}"
-                           .format(sys.argv[1], sys.argv[2],
-                                   sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    first = session.query(State).order_by(State.id).first()
-    if first:
-        print("{}: {}".format(first.id, first.name))
-    else:
+
+    first_state = session.query(State).order_by(State.id).first()
+
+    if first_state is None:
         print("Nothing")
-    sesion.close()
+    else:
+        print("{}: {}".format(first_state.id, first_state.name))
