0e4d97a1e81af140a92b63187ae21939b572c8bd
Marcosozhol <3956@holbertonschool.com>
2022-04-15 18:13:25 -0700

    Correction code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,6 +16,5 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT * FROM `states` ORDER BY id ASC")
     [print(state) for state in cur.fetchall() if state[1][0] == "N"]
-    
     cur.close()
     db.close()
