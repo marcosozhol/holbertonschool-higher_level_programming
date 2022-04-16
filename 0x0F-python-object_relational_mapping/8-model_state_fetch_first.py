56589b89f5bafd53f5c3d42fe1ba8a2446ab0115
Marcosozhol <3956@holbertonschool.com>
2022-04-15 19:23:35 -0700

    Correction error

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
-    session = Session(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
     first = session.query(State).order_by(State.id).first()
     if first:
         print("{}: {}".format(first.id, first.name))
