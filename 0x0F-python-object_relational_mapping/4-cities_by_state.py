203798ddf6874158f0459ddfff500ab6e86422ee
Marcosozhol <3956@holbertonschool.com>
2022-04-15 18:43:49 -0700

    Correction error

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
             FROM cities LEFT JOIN states\
-            ON states.id = cities.states_id\
+            ON states.id = cities.state_id\
             ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
