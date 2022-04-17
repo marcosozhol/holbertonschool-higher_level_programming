33fc90634a7ca10448feca374378331ab9ee4f15
Marcosozhol <3956@holbertonschool.com>
2022-04-17 17:48:37 -0300

    correction error

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 const square = require('./5-square');
 
 class Square extends square {
-  this.charPrint (c) {
+  charPrint(c) {
     charPrint(c) {
       if (c === undefined) {
         console.log(('X'.repeat(this.width) + '\n').repeat(this.height - 1) + 'X'.repeat(this.width));
