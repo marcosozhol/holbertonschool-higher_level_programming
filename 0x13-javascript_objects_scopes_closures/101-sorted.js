43890a95a3efb7769aad192e7d3c85ea051b73e8
Marcosozhol <3956@holbertonschool.com>
2022-04-17 19:25:59 -0300

    correction code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
 
+//  script that imports a dictionary of occurrences by 
+// user id and computes a dictionary of user ids by occurrence.
+
 const { dict } = require('./101-data');
 
 const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
