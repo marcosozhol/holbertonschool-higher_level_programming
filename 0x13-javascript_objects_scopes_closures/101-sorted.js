7d417b83c64d3e56701eff5ef052b3bd7f0e10c4
Marcosozhol <3956@holbertonschool.com>
2022-04-17 19:18:44 -0300

    Correction code

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const {dict} = require('./101-data');
+const { dict } = require('./101-data');
 
 const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
   acc[value] = acc[value] ? [...acc[value], key] : [key];
