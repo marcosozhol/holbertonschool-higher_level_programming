a566057a2bfb73bb9ebc81fd519fcdbc4d25ef74
Marcosozhol <3956@holbertonschool.com>
2022-04-17 18:42:00 -0300

    correction code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 
 exports.esrever = function (list) {
   const arr = [];
-  for (let i = list.length -1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     arr.push(list[i]);
   }
   return (arr);
-}
+};
