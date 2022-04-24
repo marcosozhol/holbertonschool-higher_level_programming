3d3816b5459a85508273e7d289f9e5f250b6e7c2
Marcosozhol <3956@holbertonschool.com>
2022-04-24 02:31:21 -0300

    Correction error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
-    
+  
 
-if __name___ == "__main__":
+if __name__ == "__main__":
     import urllib.request
     
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
