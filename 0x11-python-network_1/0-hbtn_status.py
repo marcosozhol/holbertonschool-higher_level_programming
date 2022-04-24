f3aaa9cbe8f167b4554c1971f124fde61c2d5a8d
Marcosozhol <3956@holbertonschool.com>
2022-04-24 05:45:58 +0000

    Correction error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
-  
+
 
 if __name__ == "__main__":
     import urllib.request
-    
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        the_page = response.read()
+        page = response.read()
         print('Body response:')
-        print('\t- type: {}'.format(type(the_page)))
-        print('\t- content: {}'.format(type(the_page)))
-        print('\t- utf8 content: {}'.format(the_page.decode("utf8", "replace")))
+        print('\t- type: {}'.format(type(page)))
+        print('\t- content: {}'.format(page))
+        print('\t- utf8 content: {}'.format(page.decode("utf8", "replace")))
