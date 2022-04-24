72c3ae9db5b152eae9f06b7012831c51e839b5a4
Marcosozhol <3956@holbertonschool.com>
2022-04-24 02:19:10 -0300

    Task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that fetches https://intranet.hbtn.io/status"""
+    
+
+if name == "__main__":
+    import urllib.request
+    
+    with urllib.request.urlopen(https://intranet.hbtn.io/status) as response:
+        the_page = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(type(html)))
+        print('\t- utf8 content: {}'.format(html.decode("utf8, replace")))
