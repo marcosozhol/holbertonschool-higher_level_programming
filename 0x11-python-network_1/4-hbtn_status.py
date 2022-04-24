7bd46fb8474d165dd8279b12a6a82db41de659ac
Marcosozhol <3956@holbertonschool.com>
2022-04-24 11:34:17 -0300

    Correction error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     response = requests.get('https://intranet.hbtn.io/status')
 
     print('Body response:')
-    print('\t - type: {}'.format(type(response.text)))
-    print('\t - content: {}'.format(response.text))
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
