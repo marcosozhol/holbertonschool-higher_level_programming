c0418b1a08b6a022c1b828bbd0ddb1fe0be59b45
Marcosozhol <3956@holbertonschool.com>
2022-04-24 11:26:00 -0300

    Correction error

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,7 +8,9 @@ You are not allow to import packages other than requests
 
 if __name__=="__main__":
     import requests
+
     response = requests.get("https://intranet.hbtn.io/status")
+
         print('Body response:')
         print('\t - type: {}'.format(type(response.text)))
         print('\t - content: {}'.format(response.text))
