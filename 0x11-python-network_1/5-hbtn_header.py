109175736304909ba3907a1d206f341fe77caa48
Marcosozhol <3956@holbertonschool.com>
2022-04-24 19:00:55 -0300

    Correction error

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     import requests
     import sys
     
-    response = requests.get('X-Request-Id') as response
-    print(response)
+    response = requests.get(sys.argv[1])
+    print(response.headers.get('X-Request-Id'))
