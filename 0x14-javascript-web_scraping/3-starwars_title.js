8b1b60ed51774a48d58cd89f25738a0484018a0f
Marcosozhol <3956@holbertonschool.com>
2022-05-17 20:43:43 -0300

    Correction error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + Process.argv[2];
-request(url, function(error, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
