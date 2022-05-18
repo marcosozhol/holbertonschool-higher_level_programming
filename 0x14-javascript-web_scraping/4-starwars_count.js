e13b4ddf9e7463211e0ed035a3cafae1bf3311bd
Marcosozhol <3956@holbertonschool.com>
2022-05-17 23:20:59 -0300

    Correction error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const result = JSON.parse(body).result;
+    const results = JSON.parse(body).results;
     console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
